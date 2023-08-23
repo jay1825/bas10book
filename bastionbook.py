19e6ae6331cec301d7ba5f6fae4ecded05d7d503
root <root@ip-172-31-26-40.us-west-2.compute.internal>
2023-08-23 11:04:09 +0000

    updated api url

--- bastionbook.py
+++ bastionbook.py
@@ -42,7 +42,7 @@ def register():
             print((inst_name, user, ipaddr, pvt_key))
 
             try:
-                url = 'https://25a55it941.execute-api.us-west-2.amazonaws.com/test/dynamodbmanager'
+                url = 'https://mygq8yd0jg.execute-api.us-west-2.amazonaws.com/test/dynamodbmanager'
                 new_entry = {
                     'operation': str(operation),
                     'payload': {
